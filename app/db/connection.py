8daef4fe1e490606d55d7773242faebfa51b1230
NavneetSinh Parmar <navneetparmar8952@gmail.com>
2026-02-21 18:17:38 +0530

    removed depoye to the direst server concept

--- app/db/connection.py
+++ app/db/connection.py
@@ -7,5 +7,6 @@ load_dotenv()  # load .env file
 MONGO_URL = os.getenv("MONGO_URL")
 DB_NAME = os.getenv("DATABASE_NAME")
 
+print(f"Connecting to MongoDB at: {MONGO_URL}, Database: {DB_NAME}")
 client = AsyncIOMotorClient(MONGO_URL)
 db = client[DB_NAME]
